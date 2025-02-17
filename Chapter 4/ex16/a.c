a711c3b4f3396c24ae116405749cbf38a88aeaf1
Eduardo499 <dudu.araujo.ferreira@outlook.com>
2025-02-17 15:25:27 -0300

    ex16

---
+++ Chapter 4/ex16/a.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void) {
+    int count = 1;
+
+    for (int i = 1; i <= 10; i++) {
+        for (int j = 1; j <= count; j++) {
+            printf("*");
+        }
+        printf("\n");
+        count++;
+    }
+
+    return 0;
+}
---
+++ Chapter 4/ex16/c.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(void) {
+    int count = 10;
+
+    for (int i = 1; i <= 10; i++) {
+        for (int s = 0; s < 10 - count; s++) {
+            printf(" ");
+        }
+
+        for (int j = 1; j <= count; j++) {
+            printf("*");
+        }
+        printf("\n");
+        count--;
+    }
+
+    return 0;
+}
