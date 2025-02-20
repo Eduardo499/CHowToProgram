d8f09baca6f145fb2570297d36458f599a622def
Eduardo499 <dudu.araujo.ferreira@outlook.com>
2025-02-20 14:34:09 -0300

    ex11

---
+++ Chapter 9/ex11/ex11.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    printf("Single quote: \' \n");
+    printf("Double quote: \" \n");
+    printf("Question mark: \? \n");
+    printf("Backslash: \\ \n");
+    printf("Alert (beep): \a \n");
+    printf("Backspace: X\bY \n");
+    printf("Newline: Line1\nLine2\n");
+    printf("Carriage return: Line1\rLine2\n");
+    printf("Tab: Before\tAfter\n");
+
+    return 0;
+}
